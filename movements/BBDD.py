c48068fd9f115a07bb034404d094a6b81044b2d3
unknown <charly_lagar@hotmail.com>
2021-02-28 16:05:51 +0100

    sin HTML/CSS

---
+++ movements/BBDD.py
@@ -0,0 +1,111 @@
+import sqlite3
+import datetime
+import apicoin
+
+#Funcion que obtiene el numero total de monedas de un tipo en la BD
+def numbersinglecoin(symbolfrom):
+	try:
+		conn = sqlite3.connect("movements/data/movimientos.db")
+		conn.row_factory = sqlite3.Row
+		fromcoin = conn.execute('SELECT SUM(form_quantity) as RESTA FROM MOVEMENTS WHERE from_currency=\''+str(symbolfrom)+'\'').fetchall()
+		tocoin = conn.execute('SELECT SUM(to_quantity) as SUMA FROM MOVEMENTS WHERE TO_currency=\''+str(symbolfrom)+'\'').fetchall()
+	except:
+			print("Base de datos no se encuentra")
+	finally:
+			conn.close()
+	suma=0
+	resta=0
+	if(fromcoin[0]['RESTA']):
+		resta=-fromcoin[0]['RESTA']
+	if(tocoin[0]['SUMA']):
+		suma=tocoin[0]['SUMA']
+	return suma+resta
+
+#Funcion que comprueba si la cantidad de monedas que tiene es suficiente para gastar
+def checkcoins(symbolfrom,ncoinsfrom):
+
+	check=False
+	#EUR son infinitos
+	if symbolfrom=="EUR" :
+		check=True
+	#Si numero de monedas a utilizar es menor que el que numero de monedas que tienes
+	elif(float(ncoinsfrom)<numbersinglecoin(symbolfrom)):
+		check=True
+
+	return check
+#Funcion que llama a la funcion agregar nuevo movimiento, comprobando previamente si tiene suficientes monedas
+#Devolvera valor booleano con el resultado de la operacion
+def checkaddmove(formfromsymbol,numbercoins,fromto,totalcoins):
+
+	check=checkcoins(formfromsymbol,numbercoins)
+	if(check):
+		now=datetime.datetime.now()
+		today = now.strftime("%d-%m-%Y")
+		time = now.strftime("%H:%M:%S")
+		addMove(today,time,formfromsymbol,numbercoins,fromto,totalcoins)
+	return check
+
+#Funcion que obtiene de la BD la cantidad de un tipo de moneda actual real invertido
+def totalinvertidocoin(symbol):
+	try:
+		conn = sqlite3.connect("movements/data/movimientos.db")
+		conn.row_factory = sqlite3.Row
+		coininvertidas = conn.execute('SELECT SUM(form_quantity) as INVERTDO FROM MOVEMENTS WHERE from_currency=\''+str(symbol)+'\'').fetchall()
+	except:
+			print("Base de datos no se encuentra")
+	finally:
+			conn.close()
+	inversion=0
+	if(coininvertidas[0]['INVERTDO']):
+		inversion=coininvertidas[0]['INVERTDO']
+	return inversion
+
+#En nuestro caso el total de invertido solo corresponde a la moneda euro, que es desde proviene nuestra inversion
+def totalinvertidos():
+	invertido=0
+	invertido=invertido + totalinvertidocoin("EUR")
+	return invertido
+
+#Funcion que devuelve todo los beneficios de toda las BD: saldo de beneficios sacados de EUR y ganancias (presentes) de cada moneda
+def currentvalue():
+	total=0
+	#Value coins estan todas menos EUR
+	valuecoins= ["BTC","ETH","LTC","BNB","EOS","XLM","TRX","XRP","BCH","USDT","BSV","ADA"]
+	
+	#Comprobar las cuentas de la moneda EUR, si valor >0 entonces son ganancias
+	profit=numbersinglecoin("EUR")
+	if(profit>0):
+		total=profit
+	#Comprobar cada monedas e ir sumando al total
+	for coin in valuecoins:
+		profitcoin=numbersinglecoin(coin)
+		if(profitcoin>0):
+			pu,profitEUR=apicoin.computevaluecoin(coin,"EUR",profitcoin)
+			total=total+profitEUR
+	return total
+
+#Funcion para obetenr todas las filas de la BD
+def get_data():
+	post=[]
+	try:
+		conn = sqlite3.connect("movements/data/movimientos.db")
+		conn.row_factory = sqlite3.Row
+		posts = conn.execute('SELECT * FROM MOVEMENTS').fetchall()
+	except:
+		print("Base de datos no se encuentra")
+	finally:
+		conn.close()
+	return posts
+
+#Funcion que crea un nuevo registro en la tabla MOVEMENTS
+def addMove(date,time,from_currency,form_quantity,to_currency,to_quantity):
+	try:
+		miConexion=sqlite3.connect("movements/data/movimientos.db")
+		miCursor2 = miConexion.cursor()
+
+		data = date,time,from_currency,form_quantity,to_currency,to_quantity
+
+		miCursor2.execute("INSERT INTO MOVEMENTS VALUES (NULL,?,?,?,?,?,?)",(data))
+		miConexion.commit()
+	finally:
+		miConexion.close()
---
+++ movements/views.py
@@ -0,0 +1,73 @@
+from movements import app
+from flask import render_template, request,redirect
+import BBDD
+import apicoin
+
+#DBFILE = app.config['DBFILE']
+
+#Pagina principal a la que pasaremos todos los datos de la tabla MOVEMENTS para que la muestre por pantalla
+@app.route('/') 
+def index():
+	posts = BBDD.get_data()
+	return render_template('index.html', posts=posts)
+
+#Pagina Purchase donde se realizará la compra de las monedas
+	#Post: Obtendremos el boton pra saber la accion correspondiente a utilizar
+@app.route('/purchase',methods=['GET','POST']) 
+def purchase():
+	if request.method == 'POST':
+		formfromsymbol=request.form["formfrom"]
+		fromtosymbol=request.form["fromto"]
+		numbercoins=request.form["cryptonumber"]
+
+		if numbercoins == "" or numbercoins == 0:
+			return render_template('purchase.html',coinerror=True)
+
+		if(request.form['accion']=="calculadora"):
+
+			#En caso de que no sean iguales calcularemos los datos correspondientes llamando a la API (apicoin.py)
+			if not(formfromsymbol==fromtosymbol):
+				pu,totalcoins=apicoin.computevaluecoin(formfromsymbol,fromtosymbol,numbercoins)
+				return render_template('purchase.html',PU=pu,totalcoins=totalcoins,formfromsymbol=formfromsymbol,fromtosymbol=fromtosymbol,numbercoins=numbercoins)
+
+			#En caso de sean iguales volvemos a la pagina con el error correspondiente activado	
+			return render_template('purchase.html',paramerror=True,formfromsymbol=formfromsymbol,fromtosymbol=fromtosymbol)
+
+		if(request.form['accion']=="borrar"):
+			return render_template('purchase.html',PU=" ",formfromsymbol="EUR",fromtosymbol="EUR",numbercoins=" ",totalcoins=" ")
+		if(request.form['accion']=="comprar"):
+			#Para comprobar que el usuario no ha cambiado los datos o la moneda cambia de valor volvemos a calcular el precio actual y lo comparamos
+			pu,totalcoins=apicoin.computevaluecoin(formfromsymbol,fromtosymbol,numbercoins)
+			if((pu!=float(request.form['PU'])) or (totalcoins!=float(request.form['totalcoins']))):
+				#Si ha cambiado entonces volvemos a la pagina con el error correspondiente
+				return render_template('purchase.html', changeerror=True,numbercoins=0,PU=0,totalcoins=0)
+			else:
+				#Sino enviamos los datos para añadir un movimiento a la base de datos
+				answer=BBDD.checkaddmove(formfromsymbol,numbercoins,fromtosymbol,totalcoins)
+				#En caso de que se efectue correctamente se vuelve al home
+				if answer:
+					return redirect('/')
+				#En caso de no tener monedas suficientes volvemos a la pagina con el error correspondiente
+				else:
+					return render_template('purchase.html',coinerror=True)
+
+
+	return render_template('purchase.html')
+
+
+#Pagina donde mostrara el estado de nuestros movimientos
+@app.route('/status',methods=['GET','POST'])
+def status():
+	if request.method == 'POST':
+		#Boton volver
+		return redirect('/')
+
+	inversion=BBDD.totalinvertidos()
+	currentvalue=BBDD.currentvalue()
+	return render_template('status.html',valoractual=currentvalue,inversion=inversion)
+
+#En caso de no encontrar la pagina por un error 404 avisar en una nueva pagina
+@app.errorhandler(404)
+def page_not_found(error):
+	return '<h1>Página no encontrada...</h1>',404
+
